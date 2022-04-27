b984e0cf2a23ea20246503d0dabbdad0138e8f9f
Thomas DUJARDIN <thomas.dujardin2.etu@ayou04.iut-infobio.priv.univ-lille1.fr>
2022-04-27 11:46:08 +0200

    modif graphe

--- src/tpsae/ExoAffectation2.java
+++ src/tpsae/ExoAffectation2.java
@@ -4,24 +4,25 @@ import java.util.ArrayList;
 import fr.ulille.but.sae2_02.graphes.CalculAffectation;
 import fr.ulille.but.sae2_02.graphes.GrapheNonOrienteValue;
 
-public class ExoAffectation{
+public class ExoAffectation2{
 	public static void main(String [] args){
 		double [][] counts = new double [][]{
 
-			new double [] {3.5,-2.1,1.2,-3.7,100,100,100}, //claude
-			new double [] {6.4,0.8,4.1,-1.2,100,100,100}, //Madeleine
-			new double [] {0.6,-5,-1.7,-7,100,100,100}, //Saline
-			new double [] {5.1,-0.5,2.8,-2.5,100,100,100}}; //huge
-			//new double [] {-4,-9.6,-6.3,-11.6,100,100,100}, //lucas
+			new double [] {5,100,10,15,0},
+			new double [] {5,50,10,100,0},
+			new double [] {0,50,5,100,10}, //Madeleine
+			new double [] {0,50,5,100,10}, //huge
+			new double [] {0,50,5,100,10}}; //lucas
 			//new double [] {0.8,-4.8,-1.5,-6.8,100,100,100}, //david
 			//new double [] {2.8,-2.8,0.5,-4.8,100,100,100}}; //amelie
 
 
 			ArrayList<String> tutorer = new ArrayList<>();
-			tutorer.add("Claude");
-			tutorer.add("Madeleine");
-			tutorer.add("Saline");
 			tutorer.add("huge");
+			tutorer.add("Madeleine");
+			tutorer.add("Alexandria");
+			tutorer.add("Sabine");
+			tutorer.add("Lucas");
 			//tutorer.add("lucas");
 			//tutorer.add("david");
 			//tutorer.add("amelie");
@@ -30,6 +31,8 @@ public class ExoAffectation{
 			tuteur.add("François");
 			tuteur.add("Josphe");
 			tuteur.add("Martin");
+			
+			tuteur.add("Therese");
 			tuteur.add("maurice");
 			//tuteur.add("nul");
 			//tuteur.add("nul1");
@@ -43,8 +46,8 @@ public class ExoAffectation{
 				graphe.ajouterSommet(t);
 			}
 
-			for(int ip=0; ip<4; ip++){
-				for(int it=0; it<4; it++){
+			for(int ip=0; ip<5; ip++){
+				for(int it=0; it<5; it++){
 					graphe.ajouterArete(tutorer.get(ip), tuteur.get(it), counts[ip][it]);
 				}
 			}
