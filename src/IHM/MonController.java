163ae52ebe9b404d85de0005305acf0fcfa25a1f
dujardint <94767285+dujardint@users.noreply.github.com>
2022-05-25 22:15:58 +0200

    ihm-maj masquage des crochets [Etudiant] ==> Etudiant

--- src/IHM/MonController.java
+++ src/IHM/MonController.java
@@ -46,7 +46,7 @@ public class MonController {
 		listeTutore.getSelectionModel().getSelectedItems().addListener(new MonListChangeListener());
 
 		listeTuteur.getSelectionModel().getSelectedItems().addListener(new MonListChangeListener2());
-		
+
 		listeTutorat.getSelectionModel().getSelectedItems().addListener(new MonListChangeListener3());
 
 	}
@@ -54,18 +54,24 @@ public class MonController {
 	class MonListChangeListener implements ListChangeListener<String> {
 		public void onChanged(Change<? extends String> report) {
 			contenuTutore.setText("" + report.getList());
+			String texte = contenuTutore.getText().substring(1, contenuTutore.getText().length()-1);
+			contenuTutore.setText(texte);
 		}
 	}
 
 	class MonListChangeListener2 implements ListChangeListener<String> {
 		public void onChanged(Change<? extends String> report) {
 			contenuTuteur.setText(""+ report.getList());
+			String texte = contenuTuteur.getText().substring(1, contenuTuteur.getText().length()-1);
+			contenuTuteur.setText(texte);
 		}
 	}
-	
+
 	class MonListChangeListener3 implements ListChangeListener<String> {
 		public void onChanged(Change<? extends String> report) {
 			boxCouple.setText(""+ report.getList());
+			String texte = boxCouple.getText().substring(1, boxCouple.getText().length()-1);
+			boxCouple.setText(texte);
 		}
 	}
 
@@ -74,58 +80,64 @@ public class MonController {
 		//si rien selectionner on alerte l'utilisateur
 		if(contenuTutore.getText().equals("Selectionner un tutore pour afficher ses détails") || 
 				contenuTuteur.getText().equals("Selectionner un tuteur pour afficher ses détails.") ||
+				contenuTuteur.getText().isBlank() ||
+				contenuTutore.getText().isBlank() ||
 				contenuTutore.getText().equals(contenuTuteur.getText())){
 			Alert alert = new Alert(AlertType.ERROR, "selectionner au moins 2 etudiants pour les affecter !", ButtonType.OK);
 			alert.showAndWait();
 		}
 		else {
-			//ajout des 2 personnes dans la liste tutorat   			//on prend substring pour pas prendre les crochet [...]
-			listeTutorat.getItems().add(""+contenuTutore.getText().substring(1, contenuTutore.getText().length()-1) + " - " + contenuTuteur.getText().substring(1, contenuTuteur.getText().length()-1));
+			//ajout des 2 personnes dans la liste tutorat
+			listeTutorat.getItems().add(""+contenuTutore.getText() + "-" + contenuTuteur.getText());
 
 			//suppression des 2 personnes dans les listes d'origines
-			listeTutore.getItems().remove(contenuTutore.getText().substring(1, contenuTutore.getText().length()-1));
-			listeTuteur.getItems().remove(contenuTuteur.getText().substring(1, contenuTuteur.getText().length()-1));
+			listeTutore.getItems().remove(contenuTutore.getText());
+			listeTuteur.getItems().remove(contenuTuteur.getText());
 		}
 
 	}
 
-	
+
 	public void pressedButtonSuppriner(ActionEvent event) {	
 		//si rien selectionner on alerte l'utilisateur
-		
-		int idx = 0;
-		
-		for(int i =0 ; i<boxCouple.getText().length();i++) {
-			if(boxCouple.getText().charAt(i)== '-') {
-				idx = i;
+		if((boxCouple.getText().equals(("Selectionner un couple pour afficher ses détails"))) || 
+				(boxCouple.getText().equals(("[]")))){
+			Alert alert = new Alert(AlertType.ERROR, "selectionner un couple d'etudiants pour les supprimer !", ButtonType.OK);
+			alert.showAndWait();
+		}
+		else {
+			int idx = 0;
+			for(int i =0 ; i<boxCouple.getText().length();i++) {
+				if(boxCouple.getText().charAt(i)== '-') {
+					idx = i;
+				}
 			}
-		}
-		
-		
-		
-			//ajout des 2 personnes dans la liste tutorat   			//on prend substring pour pas prendre les crochet [...]
-			listeTutore.getItems().add(boxCouple.getText().substring(1,idx));
-			listeTuteur.getItems().add(boxCouple.getText().substring(idx+2, boxCouple.getText().length()-1));
+
+			//ajout des 2 personnes dans la liste tutorat   			//on prend substring pour separer le tutore du tuteur
+			listeTutore.getItems().add(boxCouple.getText().substring(0, idx));
+			listeTuteur.getItems().add(boxCouple.getText().substring(idx+1, boxCouple.getText().length()));
 			//suppression des 2 personnes dans les listes d'origines
-			listeTutorat.getItems().remove(boxCouple.getText().substring(1, boxCouple.getText().length()-1));
-			
-		}
+			listeTutorat.getItems().remove(boxCouple.getText());
+		}	
+	}
 
 
 
 	public void pressedButtonExclureTutore(ActionEvent event) {
-		listeTutore.getItems().remove(contenuTutore.getText().substring(1, contenuTutore.getText().length()-1));
 		if(listeTutore.getItems().isEmpty()) {
 			Alert alert = new Alert(AlertType.WARNING, "la liste tutore est vide !", ButtonType.OK);
 			alert.showAndWait();
+		}else {
+			listeTutore.getItems().remove(contenuTutore.getText());
 		}
 	}
 
 	public void pressedButtonExclureTuteur(ActionEvent event) {
-		listeTuteur.getItems().remove(contenuTuteur.getText().substring(1, contenuTuteur.getText().length()-1));
 		if(listeTuteur.getItems().isEmpty()) {
 			Alert alert = new Alert(AlertType.WARNING, "la liste tuteur est vide !", ButtonType.OK);
 			alert.showAndWait();
+		}else {
+			listeTuteur.getItems().remove(contenuTuteur.getText());
 		}
 	}
 
