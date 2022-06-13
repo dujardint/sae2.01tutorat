c6b21418d7e1d6920aca0332253cbf6e709dd24d
dujardint <94767285+dujardint@users.noreply.github.com>
2022-06-13 10:50:43 +0200

    icone appli

--- src/IHM/ApplicationIHM.java
+++ src/IHM/ApplicationIHM.java
@@ -7,6 +7,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class ApplicationIHM extends Application{
@@ -22,11 +23,13 @@ public class ApplicationIHM extends Application{
 
 		Scene scene = new Scene(root);
 		stage.setScene(scene);
+		stage.getIcons().add(new Image(ApplicationIHM.class.getResourceAsStream("tutorat.png")));
 		stage.setTitle("tutorat");
 		stage.show();
 	}
 
 	public static void main(String[] args) {
 		Application.launch(args);
+		
 	}
 }
